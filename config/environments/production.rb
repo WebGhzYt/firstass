bda4020603b7293c569bc6bae63306ad3dccad62
Rohit <engrohitjain5@gmail.com>
2015-01-07 19:05:29 -0800

    change in Production for css on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,18 +21,25 @@ Rails.application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or NGINX will already do this).
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  
+  config.assets.compress = true
+
+config.assets.compile = true
+
+config.assets.digest = true
+
+config.action_dispatch.x_sendfile_header = nil
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
-  config.assets.digest = true
+  
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
