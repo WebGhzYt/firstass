217264fb221d43b34f9afa377f7e43a5210a90c9
Rohit <engrohitjain5@gmail.com>
2015-01-07 18:36:44 -0800

    Firtst Commit

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,36 @@
+class Users::SessionsController < Devise::SessionsController
+  def create
+    super do |resource|
+      BackgroundWorker.trigger(resource)
+    end
+  end
+  def destroy
+          sign_out if signed_in?
+          redirect_to root_url
+  end
+  # 
+
+# before_filter :configure_sign_in_params, only: [:create]
+
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
+
+  # POST /resource/sign_in
+  # def create
+  #   super
+  # end
+
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
+
+  # protected
+
+  # You can put the params you want to permit in the empty array.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.for(:sign_in) << :attribute
+  # end
+end
